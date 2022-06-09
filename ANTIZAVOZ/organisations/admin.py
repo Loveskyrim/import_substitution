8a6174fd32d299e2cd7f24617955f5f39cff5a8c
s.marchenkov <marchenkov.s.d@gmail.com>
2022-06-09 17:29:17 +0300

    models in admin.py: organisation, product

--- ANTIZAVOZ/organisations/admin.py
+++ ANTIZAVOZ/organisations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import organisation, product
 
-# Register your models here.
+
+class OrganisationAdmin(admin.ModelAdmin):
+    "Админка организации"""
+    fields = ('OS_name', 'id_bulletin', 'url_bulletin')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    "Админка продукта"""
+    fields = ('OS_name', 'id_bulletin', 'url_bulletin', 'bulletin_author', 'status')
+
+
+admin.site.register(organisation, OrganisationAdmin)
+admin.site.register(product, ProductAdmin)
+
