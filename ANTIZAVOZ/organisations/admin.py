35f6c36008fb64fa7c238e2cdc01bfd04b3b1376
s.marchenkov <marchenkov.s.d@gmail.com>
2022-06-09 17:40:22 +0300

    models in admin.py: make migrations! and migrate!

--- ANTIZAVOZ/organisations/admin.py
+++ ANTIZAVOZ/organisations/admin.py
@@ -4,12 +4,13 @@ from .models import organisation, product
 
 class OrganisationAdmin(admin.ModelAdmin):
     "Админка организации"""
-    fields = ('OS_name', 'id_bulletin', 'url_bulletin')
+    fields = ('id_organisation', 'organisation_okved', 'organisation_category', 
+        'organisation_description', 'organisation_principal', 'organisation_link', 'organisation_sanctions')
 
 
 class ProductAdmin(admin.ModelAdmin):
     "Админка продукта"""
-    fields = ('OS_name', 'id_bulletin', 'url_bulletin', 'bulletin_author', 'status')
+    fields = ('product_name', 'product_tags', 'product_info', 'product_sanctions_import', 'product_sanctions_export')
 
 
 admin.site.register(organisation, OrganisationAdmin)
