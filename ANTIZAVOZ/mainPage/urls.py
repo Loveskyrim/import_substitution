bb66351094bde9af4e26cd093db0a0f3aa0da22a
Loveskyrim <dr.konovalov@gmail.com>
2022-06-11 10:45:59 +0300

    Небольшая правка моделей: увеличина длина полей

--- ANTIZAVOZ/mainPage/urls.py
+++ ANTIZAVOZ/mainPage/urls.py
@@ -5,9 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('index', views.product_list, name='index'),
-    path('<str:publish>/<str:slug>',
-         views.product_detail,
-         name='product_detail'),
+    path('<str:publish>/<str:prod>', views.product_detail, name='product_detail'),
     # path('', views.mainPage, name='index'),
     # path('index', views.mainPage, name='index'),
     # path('getLastProducts', views.getLastProducts, name='getLastProducts'),
