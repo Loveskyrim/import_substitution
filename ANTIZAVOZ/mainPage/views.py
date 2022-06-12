27ce662b960df0fb1ac9016a019cc132c22299ab
Loveskyrim <dr.konovalov@gmail.com>
2022-06-12 08:34:02 +0300

    Кнопк добавления организации

--- ANTIZAVOZ/mainPage/views.py
+++ ANTIZAVOZ/mainPage/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from organisations.models import *
 import datetime
+from django.contrib.auth.models import User
 
 
 def product_list(request):
-    products = product.objects.filter(status__in=('registered', 'Registered'))
+    organisations = organisation.objects.filter(author=User.objects.get(username=request.user))
+    products = product.objects.filter(author__in=organisations, status__in=('registered', 'Registered'))
     # products = product.objects.all()
-    return render(request, 'mainPage/mainPage.html', context={'products': products})
+    return render(request, 'mainPage/mainPage.html', context={'organisations': organisations, 'products': products})
 
 
 def product_detail(request, publish, prod):
-    print(publish)
     product_item = get_object_or_404(product, slug=prod,
                                      status='registered',
                                      publish__year=publish.split('.')[2],
                                      publish__month=publish.split('.')[1],
                                      publish__day=publish.split('.')[0])
     return render(request, 'organisations/product.html', context={'product_item': product_item})
+
+
+def organisation_detail(request, publish, prod):
+    organisation_item = get_object_or_404(organisation, slug=prod,
+                                          status='registered',
+                                          publish__year=publish.split('.')[2],
+                                          publish__month=publish.split('.')[1],
+                                          publish__day=publish.split('.')[0])
+    return render(request, 'organisations/organisation.html', context={'organisation_item': organisation_item})
+
+
+def organisation_create(request):
+
+    org = organisation.objects.create(
+                organisation_name=request.company_name,
+                organisation_okved=request.okved,
+                organisation_category=request.category,
+                organisation_principal=request.director,
+                organisation_inn=request.inn,
+                organisation_adress=request.adress)
+    return redirect('index')
--- ANTIZAVOZ/organisations/views.py
+++ ANTIZAVOZ/organisations/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import organisation
 
-# Отображает главную страницу сайта
+
 @login_required(login_url='/login')
 def products_request(request):
     return render(request, 'organisations/productsList.html', context={})
@@ -10,5 +10,5 @@ def products_request(request):
 
 @login_required(login_url='/login')
 def organisations_request(request):
-    employee_list = organisation.objects.all
-    return render(request, 'organisations/organisationsList.html', {"employees": employee_list})
+    organisations_list = organisation.objects.all()
+    return render(request, 'organisations/organisationsList.html', {"organisations": organisations_list})
